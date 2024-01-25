b4cc7c4b9f0d2e7c396f59257ddea1bf6c742307
livujjwal <ujjwalsingh2112@gamil.com>
2024-01-25 18:53:42 +0530

    css

--- src/components/Login.js
+++ src/components/Login.js
@@ -99,16 +99,16 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <div className="absolute w-screen h-screen">
+      <div className="absolute">
         <img
-          className="object-cover w-screen h-screen"
+          className="object-cover max-sm:h-dvh max-sm:w-dvh h-dvh w-screen "
           src="https://assets.nflxext.com/ffe/siteui/vlv3/16006346-87f9-4226-bc25-a1fb346a2b0c/9662d0fd-0547-4665-b887-771617268815/IN-en-20240115-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="bg-image"
         />
       </div>
       <form
         onSubmit={(e) => e.preventDefault()}
-        className="absolute w-4/12 h-10/12 max-lg:w-8/12 max-sm:w-10/12 bg-black bg-opacity-80 px-[5rem] py-12 my-32 mx-auto left-0 right-0 text-white rounded-md"
+        className="absolute w-4/12 lg:w-9/12 max-lg:w-9/12 max-sm:w-10/12 bg-black bg-opacity-80 px-[5rem] py-12 my-32 mx-auto left-0 right-0 text-white rounded-md"
       >
         <h1 className="text-3xl py-2 font-semibold">
           {isSignInForm ? "Sign In" : "Sign Up"}
